101c7cd18a66e2e29a7877c199b12c2533b9a453
Akkiiitj <b24cs1029@iitj.ac.in>
2025-05-20 11:22:21 +0530

    Updated Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
           <Link to="/">Home</Link>
           <Link to="/prompt">Search Here</Link>
           <Link to="/profile">Profile</Link>
-          <Link to="/editprofile">Edit Profile</Link>
+          // <Link to="/editprofile">Edit Profile</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
           <Link to="/about">About Us</Link>
