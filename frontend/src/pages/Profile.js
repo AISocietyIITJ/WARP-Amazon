b031b25b3a88f89f231bc18bf402d32df1af534b
Akkiiitj <b24cs1029@iitj.ac.in>
2025-05-20 11:09:22 +0530

    Update Profile.js

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -3,16 +3,18 @@ import "./Profile.css"
 import "./account.png"
 
 export default function ProfilePage() {
+  const userData = JSON.parse(sessionStorage.getItem('userData')) || {
+    name: "Student Name",
+    email: "student@email.com",
+  };
   return (
     <div className="profile-container">
       <div className="profile-card">
         <img src="/home/warp/amazon/integ_react/frontend/src/components/account.png" alt="profile phot" className="profile-pic"/>
         <h2 className="profile-title">User Profile</h2>
         <div className="profile-details">
-          <p><strong>Name:</strong> Student Name</p>
-          <p><strong>Email:</strong> student@email.com</p>
-          <p><strong>Branch:</strong> Chemical</p>
-          <p><strong>Roll No.:</strong> B24XY1001</p>
+          <p><strong>Name:</strong> {userData.name}</p>
+          <p><strong>Email:</strong> {userData.email}</p>
         </div>
       </div>
     </div>
